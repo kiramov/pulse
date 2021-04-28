ee5e456d30420fdc4e9c25eca84e22c983063afe
azat <kiramov8521@gmail.com>
2021-04-28 21:05:13 +1200

    Альтернативный слайдер

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
+        //dots: true,
         speed: 1200,
         //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 768,
@@ -15,4 +15,10 @@ $(document).ready(function(){
             }
         ]
       });
-  });
+  }); */
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+});
