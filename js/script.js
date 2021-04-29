15be474a7511c9b00feec48f91a72d6ed50d5e2a
azat <kiramov8521@gmail.com>
2021-04-29 17:42:09 +1200

    tabs

--- js/script.js
+++ js/script.js
@@ -21,4 +21,14 @@ const slider = tns({
     items: 1,
     slideBy: 'page',
     autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click' , function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click' , function () {
+    slider.goTo('next');
 });
